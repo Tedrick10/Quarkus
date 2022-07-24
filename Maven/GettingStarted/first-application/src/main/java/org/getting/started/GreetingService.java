cf1859e88abd1c719ce34db85bcf27629f31f5d0
Thet Tun Kyaw <thettunkyaw301098@gmail.com>
2022-07-24 12:55:39 +0630

    Created a new application, first-application.

---
+++ Maven/GettingStarted/first-application/src/main/java/org/getting/started/GreetingService.java
@@ -0,0 +1,10 @@
+package org.getting.started;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+    public String greeting(String name) {
+        return "Hello, " + name + "!";
+    }
+}
